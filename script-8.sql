108a3edce7c3866ee575a1b6e42d2ca6f0b26a11
mayo889 <69690663+mayo889@users.noreply.github.com>
2020-12-04 23:45:17 +0500

    Update script-8.sql

--- script-8.sql
+++ script-8.sql
@@ -3,6 +3,7 @@
 -- Удаление неправильного внешнего ключа
 ALTER TABLE likes DROP FOREIGN KEY likes_target_id_fk;
 
+
 -- Задание 3. Определить кто больше поставил лайков (всего) - мужчины или женщины?   
 
 SELECT 
@@ -16,22 +17,24 @@ SELECT
   ORDER BY total DESC
   LIMIT 1;
 
+ 
 -- Задание 4. Подсчитать общее количество лайков десяти самым молодым пользователям (сколько лайков получили 10 самых молодых пользователей).
 
 SELECT
   SUM(tbl.total_likes)
-FROM (
-	SELECT
-	  profiles.user_id, profiles.birthday, COUNT(likes.id) as total_likes
-	FROM profiles
-	  LEFT JOIN likes
-	    ON profiles.user_id = likes.target_id 
-	      AND target_type_id = 2
-	GROUP BY profiles.user_id
-	ORDER BY profiles.birthday DESC
-	LIMIT 10) AS tbl;
+  FROM (
+    SELECT
+      profiles.user_id, profiles.birthday, COUNT(likes.id) as total_likes
+    FROM profiles
+      LEFT JOIN likes
+        ON profiles.user_id = likes.target_id 
+          AND target_type_id = 2
+    GROUP BY profiles.user_id
+    ORDER BY profiles.birthday DESC
+    LIMIT 10) AS tbl;
 
-- Задание 5. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети
+   
+-- Задание 5. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети
 -- (критерии активности необходимо определить самостоятельно).
 
 -- Перечисленные критерии активности суммируются. Пользователи с наименьшими суммами являются менее активными
@@ -40,10 +43,17 @@ FROM (
 --   кол-во поставленных лайков
 --   кол-во загруженных медиа файлов
 --   кол-во сообществ, в которых пользователь состоит
-
+--   кол-во друзей
+ 
 SELECT
   CONCAT(first_name, ' ', last_name) AS full_name,
-  COUNT(*) AS activity
+  ( COUNT(DISTINCT(posts.id)) +
+    COUNT(DISTINCT(messages.id)) + 
+    COUNT(DISTINCT(likes.id)) + 
+    COUNT(DISTINCT(media.id)) +
+    COUNT(DISTINCT(communities_users.community_id)) +
+    COUNT(DISTINCT(friendship.user_id))
+  ) AS activity
   FROM users
     LEFT JOIN posts
       ON users.id = posts.user_id
@@ -60,5 +70,5 @@ SELECT
         OR users.id = friendship.friend_id)
         AND status_id = 2
   GROUP BY users.id
-  ORDER BY activity
-  LIMIT 10;
+  ORDER BY activity, full_name
+  LIMIT 10; 
